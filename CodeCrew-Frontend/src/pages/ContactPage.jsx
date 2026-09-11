1d28abdef5816c40ce6a8f1f0520ce8b4bdf5049
Shubh Shrivastava <shubhshri855@gmail.com>
2026-09-11 05:02:00 -0700

    changes

--- CodeCrew-Frontend/src/pages/ContactPage.jsx
+++ CodeCrew-Frontend/src/pages/ContactPage.jsx
@@ -14,12 +14,14 @@ const ContactPage = () => {
         <h1 className="text-white text-2xl  mb-2">Call Us</h1>
         <h3 className="text-zinc-200">+91 9516010257</h3>
         <h3 className="text-zinc-200">+91 7828027332</h3>
+        <h3 className="text-zinc-200">+91 9302410770</h3>
       </div>
       {/* Second Block */}
       <div className="bg-[#18181b] rounded-lg shadow-lg p-8 w-full flex flex-col gap-4">
         <h1 className="text-white text-2xl  mb-2">Email Us</h1>
         <h3 className="text-zinc-200">codecrew0001@gmail.com</h3>
         <h3 className="text-zinc-200">aadarshdangi0001@gmail.com</h3>
+        <h3 className="text-zinc-200">shubhshri855@gmail.com</h3>
       </div>
     </div>
 
